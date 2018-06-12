a9a03aec6c4ca2a207d753442429890fd3537a55
Cristina Lugo <cmlugoce@syr.edu>
2018-06-12 22:04:15 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,3 +19,11 @@ def long_planeteer_calls(calls)
   
 end
 
+def find_the_cheese (types)
+  
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  types.find do |type|
+    cheese_types.include?(type)
+  end 
+end 
